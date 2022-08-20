94fb817f68c3aa17e42316b7b2597b4b2c067d5f
Cavides <camilov370@gmail.com>
2022-08-20 14:01:20 -0500

    Ventana de cambiar contraseña hecha

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Login from "./Pages/Auth/Login/Login";
-import Register from "./Pages/Auth/Register/Register";
+// import Login from "./Pages/Auth/Login/Login";
+// import Register from "./Pages/Auth/Register/Register";
+import ChangePass from "./Pages/Auth/ChangePassword/ChanPass";
 
 function App() {
   return (
     <div> 
     
-    <Register />
+    <ChangePass />
     
       </div>
   );
--- src/Pages/Auth/ChangePassword/ChanPass.jsx
+++ src/Pages/Auth/ChangePassword/ChanPass.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
+import "./changepass.css";
+
+const nombre="Camilo";
+const apellido="Vides";
 
 function ChanPass() {
   return (
-    <div>
-    
+    <div className ="main-container">
+    {/* Username */}
+      <div className = "username"> 
+      <h1> {nombre} {apellido}</h1>
+      </div>
+
+    <div className = "changePass-form">
+    <div className = "changePass-form__title">
+    <h4>Change Password</h4> 
     </div>
+
+    <form>
+          {/* OldPassword */}
+          <div className='changePass-form__field'>
+          <label htmlFor='password'>
+          <b>Contraseña antigua</b>
+          <input className='changePass-form__field-panel' type='password' name='oldPass' placeholder="Enter your old password" required/>
+            </label>
+          </div>
+    {/* NewPassword */}
+    <div className='changePass-form__field'>
+          <label htmlFor='password'>
+          <b>Contraseña Nueva</b>
+            <input className='changePass-form__field-panel' type='password' name='newPass' placeholder="Enter your new password" required/>
+            </label>
+          </div>
+    {/* RNewPassword */}
+    <div className='changePass-form__field'>
+          <label htmlFor='password'>
+          <b>Confirmar nueva contraseña</b>
+            <input className='changePass-form__field-panel' type='password' name='rNewPass' placeholder="Repeat your new password" required/>
+            </label>
+          </div>
+    </form>
+
+    {/* submitButton */}
+    <button className="changePass-form__submit">
+        Cambiar Contraseña
+        </button>
+    {/* ForgotPassword */}
+    <div className="changePass-form__forgotpassword">
+      <h4>¿Olvidaste tu contraseña?</h4>
+      </div>
+
+    </div>
+
+    </div>
+
   )
 }
 
