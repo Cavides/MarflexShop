6ffbb7cfd94a348e1a9c248cfbbbf84fcda8a91b
Cavides <camilov370@gmail.com>
2022-08-21 19:12:15 -0500

    Home 60%

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,28 +5,70 @@ import Header from "../../Components/Header/Header";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 
+const img1="camion.png";
+const img2="dinero.png";
+const img3="IconCasa.png";
+
+const mueble1="mueble1.jpg";
+const mueble2="mueble2.jpg";
+const mueble3="mueble3.jpg";
+const mueble4="mueble4.jpg";
+
 function Home() {
   return (
     <div>
-    <div>Gran promocion con 30% de descuento</div>
+    {/* adjuntar link */}
+    <div className="aviso"><p>Envios a más de 400 municipios en Colombia</p></div>
 
     <Header />
    <Navbar />
 
-<div className="text">
-<p>Hace referencia a los acontecimientos que han marcado el devenir histórico de la actual República de Colombia, un país latinoamericano ubicado al noroccidente de América del Sur, con una superficie de 2.070.408 km² y una población de 44.091.133.[1] Colombia se conforma como estado en 1810 a partir del Virreinato de la Nueva Granada, colonia del Imperio español que había sido fundada en 1550. En 1886 toma definitivamente su nombre actual de República de Colombia. Su historia se divide por lo general en época precolombina, descubrimiento y conquista española, colonia, independencia, consolidación republicana y siglo XX. Gran parte de la historia de Colombia tiene una estrecha relación con la historia de España hasta la independencia y las historias de Ecuador, Venezuela, Panamá y de América Latina en general.
+<div className="Hero">
 
-La historia de Colombia se ha caracterizado por una acentuada diferencia de clases producto de la supremacía de los criollos blanco-mestizos sobre los nativos americanos, los esclavizados africanos y los mestizos desde el tiempo de la colonia. Los criollos lideraron tanto las gestas de independencia como la organización política y social de la naciente república, pero no lograron cambiar en mucho la situación de las comunidades indígenas, afrocolombianas (negras), mulatas y mestizas del país, lo que crearía un fuerte antagonismo social a lo largo de la historia de la república.
+    <div className="Hero__Content">
 
-El primer siglo republicano fue turbulento, con la tensión entre una concepción federalista del estado a la manera estaounidense y una concepción centralista a la manera francesa, lo que condujo al país a permanentes guerras y dio principio a los partidos conservador y liberal. La Constitución de 1886 liderada por el presidente Rafael Núñez puso fin a la hegemonía liberal y creó un estado centralista, conservador y estrictamente católico.
+      {/* Carrusel de img */}
+      <div className="slider-container">
+      <ul>
+      <li><img src={mueble1}/> </li>
+      <li><img src={mueble3} /> </li>
+      <li> <img src={mueble2} /></li>
+      <li><img src={mueble4} /> </li>
+      </ul>
+</div>
 
-Entre mediados del siglo XIX Y XX fue inaugurado en Colombia por la Guerra de los Mil Días, que debilitaría de gran manera al estado, frenaría el desarrollo económico y haría que el país perdiera a Panamá en 1903. Una primera revolución industrial colombiana se daría con el regreso de gobiernos liberales, pero la Masacre de las Bananeras evidenciaría una enorme situación de desventaja del obrero colombiano que caracterizaría el resto del siglo.
 
-Sin duda el acontecimiento más notable del siglo XX en Colombia fue el asesinato del caudillo liberal Jorge Eliecer Gaitán en un magnicidio que aún no se esclarece. Dicho evento, ocurrido el 9 de abril de 1948, precipitaría al país a una violencia inusitada que se ensañó especialmente con el campesinado y que enfrentó a muerte a los dos partidos tradicionales. El Frente Nacional, un pacto entre ambos partidos, reconcilió a los jefes, pero dejó por fuera a muchos sectores, los cuales serían la semilla de las guerrillas liberales y comunistas que durarían todo el resto del siglo. El fortalecimiento de las mafias de la droga a partir de la década del 70 pondría en jaque a la sociedad colombiana y afectaría profundamente a las clases dirigentes. Sin embargo, presiones internacionales, especialmente por parte de los Estados Unidos y la labor ética de ciertos políticos, periodistas, jueces y autoridades que no se doblegaron ante el avance de las mafias, causaron una sangrienta guerra contra el estado en la década de los 80 y especialmente hasta la muerte de Pablo Escobar, su principal líder, en 1993. El gobierno del presidente César Gaviria llevó a cabo un proceso al que llamó "apertura económica" que hizo que Colombia pasara de una economía proteccionista a una globalizada. Con Gaviria se adelantó la Constituyente en la cual se firmó una nueva Carta Política.
+      {/* Imagenes relacionadas */}
+        <div className="Hero__Image">
 
-El gobierno del presidente Andrés Pastrana adelantó los diálogos más cercanos que se hayan tenido con las guerrillas y muy especialmente con las FARC al crear zonas de despeje. Sin embargo, estas fortalecieron su accionar militar en el país por medio de atentados, secuestros, intimidación y tráfico de drogas. Por su parte, se crearon los grupos Paramilitares, especialmente bajo el liderazgo de las AUC cuyo fin era combatir a las guerrillas. Dichos grupos tuvieron el respaldo de numerosos miembros de las fuerzas militares y policiales, así como de políticos y hacendados. El accionar de los paramilitares en Colombia y sus batallas en contra de las guerrillas, creó un drama humanitario de proporciones mundiales al poner al país como uno de los primeros en número de desplazados, crímenes de guerra como masacres y terror en numerosas regiones del país.
+          <div className='Hero__image-uno'>
+          <p>Envios a casi 400 municipios en todo el terriotrio Colombiano</p>
+          <img src={img1}/>
+          </div>
 
-El estado de violencia y desesperanza llevó a que los colombianos vieran la propuesta de Seguridad democrática presentada por Álvaro Uribe como la mejor opción. Al asumir como presidente en 2001, Uribe adelantó un proceso de reinserción de los grupos paramilitares, ha doblegado la fuerza de las guerrillas y ha adelantado un intenso programa de globalización de la economía caracterizado por la firma de tratados de libre comercio con otros países. El presidente impulsó una reforma política que determinó una modificación en la constitución de 1991, esta reforma establecía la posibilidad de reelección inmediata para el máximo dirigente del estado, así, pudo presentarse como candidato en 2006 y ser reelegido en un segundo término (2006-2010). Sin embargo, su gobierno ha sido el centro de álgidos debates dentro y fuera del país: para muchos observadores ha devuelto la paz y la seguridad a Colombia mientras devolvió la credibilidad a instituciones como la policía y el ejército. Para otros observadores existen varios problemas: los crecientes escándalos de corrupción en las instituciones y referentes a la modificación del artículo de la constitución que permitió la reelección inmediata del presidente y el aumento en la violación de los derechos humanos por parte de los actores armados. </p>
+          <div className='Hero__image-uno'>
+          <p>Ahorra comprando los mejores productos para el hogar</p>
+          <img src={img2}/>
+          </div>
+
+          <div className='Hero__image-uno'>
+          <p>Todo lo que necesitas para la comodidad en tu casa</p>
+          <img src={img3}/>
+          </div>
+
+
+
+
+          </div>
+      {/* productos aleatorios */}
+      
+
+      {/* Imagen de Credito */}
+
+
+    </div>
+
+
 </div>
 
 <Footer />
