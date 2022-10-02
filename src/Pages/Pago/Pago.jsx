3c16a5106ebe5441301be36d89ccb46c29499ccb
Cavides <camilov370@gmail.com>
2022-10-02 13:13:01 -0500

    Agregar productos a Carrito

--- src/Pages/Pago/Pago.jsx
+++ src/Pages/Pago/Pago.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToCart,
+  clearCart,
+  decreaseCart,
+  getTotals,
+  removeFromCart,
+} from "../../slices/cartSlice";
 
 import Header from "../../Components/Header/Header";
 import Navbar from "../../Components/Navbar/Navbar";
@@ -6,13 +15,33 @@ import Footer from "../../Components/Footer/Footer";
 
 import "./pago.css";
 
-const imgpago= "https://res.cloudinary.com/ds9rxxr5l/image/upload/v1661626273/imagenes/pago_sym54q.png"
+const imgpago =
+  "https://res.cloudinary.com/ds9rxxr5l/image/upload/v1661626273/imagenes/pago_sym54q.png";
 
 function Pago() {
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+  dispatch(getTotals());
+}, [cart, dispatch]);
+
+const handleAddToCart = (product) => {
+  dispatch(addToCart(product));
+};
+const handleDecreaseCart = (product) => {
+  dispatch(decreaseCart(product));
+};
+const handleRemoveFromCart = (product) => {
+  dispatch(removeFromCart(product));
+};
+const handleClearCart = () => {
+  dispatch(clearCart());
+};
   return (
     <div>
-    <Header />
-    <Navbar />
+      <Header />
+      <Navbar />
       <div className="main-containerPago">
         <div className="pago-form">
           {/* title */}
@@ -137,9 +166,9 @@ function Pago() {
             </div>
 
             <div className="pago-form__subtitle">Información de pago</div>
-            
+
             <div className="pago-form__image">
-            <img src={imgpago} />
+              <img src={imgpago} />
             </div>
 
             <div className="pago-form__field">
@@ -202,7 +231,53 @@ function Pago() {
         </div>
       </div>
 
-      <Footer/>
+
+      <div className="pago-form__products">
+      <div>
+          <div className="cart-items">
+            {cart.cartItems &&
+              cart.cartItems.map((cartItem) => (
+                <div className="cart-item" key={cartItem.id}>
+                  <div className="cart-product">
+                    <img src={cartItem.image} alt={cartItem.title} />
+                    <div>
+                      <h3>{cartItem.title}</h3>
+                      <button onClick={() => handleRemoveFromCart(cartItem)}>
+                        Quitar
+                      </button>
+                    </div>
+                  </div>
+                  <div className="cart-product-price">${cartItem.price}</div>
+                  <div className="cart-product-quantity">
+                    <button onClick={() => handleDecreaseCart(cartItem)}>
+                      -
+                    </button>
+                    <div className="count">{cartItem.cartQuantity}</div>
+                    <button onClick={() => handleAddToCart(cartItem)}>+</button>
+                  </div>
+                  <div className="cart-product-total-price">
+                    ${cartItem.price * cartItem.cartQuantity}
+                  </div>
+                </div>
+              ))}
+          </div>
+          <div className="cart-summary">
+            <button className="clear-btn" onClick={() => handleClearCart()}>
+              Limpiar carrito
+            </button>
+            <div className="cart-checkout">
+              <div className="subtotal">
+                <span>Subtotal</span>
+                <span className="amount">${cart.cartTotalAmount}</span>
+              </div>
+              </div>
+              </div>
+              </div>
+
+      </div>
+
+
+      <Footer />
     </div>
   );
 }
