b8cef3b1643ec5c903ba2124d74b9108118641e1
Cavides <camilov370@gmail.com>
2022-11-15 17:48:35 -0500

    arreglo

--- src/Pages/Products/ProductDetail/ProductDetail.jsx
+++ src/Pages/Products/ProductDetail/ProductDetail.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import "./productDetail.css";
+import Header from "../../../Components/Header/Header";
+import Navbar from "../../../Components/Navbar/Navbar";
+import Footer from "../../../Components/Footer/Footer";
 
 import { getProduct } from '../../../Services/Products_services';
 
@@ -18,6 +21,9 @@ function Detail() {
   }, []);
 
   return (
+    <div>
+    <Header />
+    <Navbar />
     <div className='detail'>
       <h1 className='detail__title'>{`${product.title}`}</h1>
       <div className='detail__info'>
@@ -25,13 +31,16 @@ function Detail() {
           <img className='detail__img' src={product.imagen} />
         </div>
         <div className='detail__text'>
-          <h3 className='detail__category'>{product.category}</h3>
+          <h3 className='detail__category'>Categoria : {product.category}</h3>
+          <h3 className='detail__category'>Tipo : {product.type}</h3>
           <p className='detail__description'>{product.descripcion}</p>
           <div className='detail__down'>
-            <h3 className='detail__price'>{`$${product.price}`}</h3>
+            <h3 className='detail__price'>Precio : {`$${product.price}`}</h3>
           </div>
         </div>
       </div>
+    </div>
+    <Footer/>
     </div>
   );
 }
